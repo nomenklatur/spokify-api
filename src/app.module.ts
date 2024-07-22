9a2873f5e938cff0e649d2b7b047294539357aa9
Dimas <dimasekaputra2000@gmail.com>
2024-07-22 11:40:36 +0700

    chore: folder restructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
-import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware.js';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware.js';
 
 @Module({
   imports: [SongsModule],
